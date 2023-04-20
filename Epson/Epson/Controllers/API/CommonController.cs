baebc94238fa6f07628fd73184d7df98da04f2a5
phengwei <56812779+phengwei@users.noreply.github.com>
2023-04-20 16:39:10 +0800

    add swashbuckle for api documentation

---
+++ Epson/Epson/Controllers/API/CommonController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EpsonPortal.Controllers.API
+{
+    [Route("api/[controller]")]
+    public class CommonController : Controller
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
--- Epson/Epson/Program.cs
+++ Epson/Epson/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v3", new OpenApiInfo { Title = "Epson APIs", Version = "v3" });
+});
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -12,6 +19,12 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v3/swagger.json", "My API v1");
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
