770092e2ef3cf229d25cf699dec5b557111ca758
phengwei <56812779+phengwei@users.noreply.github.com>
2023-05-08 14:52:36 +0800

    update db script

--- Epson/Epson.Data/SQLScripts/epson-db.sql
+++ Epson/Epson.Data/SQLScripts/epson-db.sql
@@ -14,33 +14,127 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Dumping structure for table epsonlocal.audit_trail
-CREATE TABLE IF NOT EXISTS `audit_trail` (
-  `id` int NOT NULL DEFAULT '0',
+
+-- Dumping database structure for epsonlocaldb
+CREATE DATABASE IF NOT EXISTS `epsonlocaldb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `epsonlocaldb`;
+
+-- Dumping structure for table epsonlocaldb.aspnetroleclaims
+CREATE TABLE IF NOT EXISTS `aspnetroleclaims` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `RoleId` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `ClaimType` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `ClaimValue` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table epsonlocaldb.aspnetroles
+CREATE TABLE IF NOT EXISTS `aspnetroles` (
+  `Id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Name` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `NormalizedName` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `ConcurrencyStamp` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table epsonlocaldb.aspnetuserclaims
+CREATE TABLE IF NOT EXISTS `aspnetuserclaims` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `UserId` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `ClaimType` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `ClaimValue` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table epsonlocaldb.aspnetuserlogins
+CREATE TABLE IF NOT EXISTS `aspnetuserlogins` (
+  `LoginProvider` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `ProviderKey` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `ProviderDisplayName` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `UserId` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  PRIMARY KEY (`LoginProvider`,`ProviderKey`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table epsonlocaldb.aspnetuserroles
+CREATE TABLE IF NOT EXISTS `aspnetuserroles` (
+  `UserId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `RoleId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`UserId`,`RoleId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table epsonlocaldb.aspnetusers
+CREATE TABLE IF NOT EXISTS `aspnetusers` (
+  `Id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Discriminator` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `UserName` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `NormalizedUserName` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `Email` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `NormalizedEmail` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `EmailConfirmed` tinyint(1) NOT NULL,
+  `PasswordHash` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `SecurityStamp` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `ConcurrencyStamp` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `PhoneNumber` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `PhoneNumberConfirmed` tinyint(1) NOT NULL,
+  `TwoFactorEnabled` tinyint(1) NOT NULL,
+  `LockoutEnd` datetime(6) DEFAULT NULL,
+  `LockoutEnabled` tinyint(1) NOT NULL,
+  `AccessFailedCount` int NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table epsonlocaldb.aspnetusertokens
+CREATE TABLE IF NOT EXISTS `aspnetusertokens` (
+  `UserId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `LoginProvider` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Value` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  PRIMARY KEY (`UserId`,`LoginProvider`,`Name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table epsonlocaldb.audittrail
+CREATE TABLE IF NOT EXISTS `audittrail` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `entityId` int DEFAULT NULL,
   `entity` varchar(30) DEFAULT NULL,
   `actionTime` datetime DEFAULT NULL,
   `actor` varchar(50) DEFAULT NULL,
   `action` varchar(30) DEFAULT NULL,
   `actionDetails` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `createdOnUTC` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `createdOnUTC` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table epsonlocal.category
+-- Dumping structure for table epsonlocaldb.category
 CREATE TABLE IF NOT EXISTS `category` (
-  `id` int NOT NULL DEFAULT '0',
-  `name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table epsonlocal.email_account
-CREATE TABLE IF NOT EXISTS `email_account` (
-  `id` int NOT NULL DEFAULT '0',
+-- Dumping structure for table epsonlocaldb.emailaccount
+CREATE TABLE IF NOT EXISTS `emailaccount` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `email` varchar(50) DEFAULT NULL,
-  `username` varchar(20) DEFAULT NULL,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `password` varchar(20) DEFAULT NULL,
   `incomingProtocol` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `incomingServer` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
@@ -51,21 +145,22 @@ CREATE TABLE IF NOT EXISTS `email_account` (
   `outgoingPort` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `outgoingSsl` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `isActive` bit(1) DEFAULT NULL,
-  `createdById` int DEFAULT NULL,
-  `updatedById` int DEFAULT NULL,
+  `createdById` longtext,
+  `updatedById` longtext,
   `createdOnUTC` datetime DEFAULT NULL,
   `updatedOnUTC` datetime DEFAULT NULL,
-  `lastCheckedTime` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `lastCheckedTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table epsonlocal.email_queue
-CREATE TABLE IF NOT EXISTS `email_queue` (
-  `id` int NOT NULL DEFAULT '0',
+-- Dumping structure for table epsonlocaldb.emailqueue
+CREATE TABLE IF NOT EXISTS `emailqueue` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `priority` int DEFAULT '0',
-  `from` varchar(30) DEFAULT NULL,
-  `to` varchar(30) DEFAULT NULL,
+  `fromemail` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `toemail` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `cc` varchar(100) DEFAULT NULL,
   `bcc` varchar(100) DEFAULT NULL,
   `subject` varchar(100) DEFAULT NULL,
@@ -75,86 +170,102 @@ CREATE TABLE IF NOT EXISTS `email_queue` (
   `sendAttempts` int DEFAULT NULL,
   `sentTime` datetime DEFAULT NULL,
   `emailAccountId` int DEFAULT NULL,
-  `createdOnUTC` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table epsonlocal.product
+-- Dumping structure for table epsonlocaldb.product
 CREATE TABLE IF NOT EXISTS `product` (
-  `id` int NOT NULL DEFAULT '0',
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   `price` decimal(20,6) DEFAULT NULL,
-  `createdById` int DEFAULT NULL,
-  `updatedById` int DEFAULT NULL,
+  `createdById` longtext,
+  `updatedById` longtext,
   `createdOnUTC` datetime DEFAULT NULL,
-  `updatedOnUTC` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `updatedOnUTC` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table epsonlocal.productcategory
+-- Dumping structure for table epsonlocaldb.productcategory
 CREATE TABLE IF NOT EXISTS `productcategory` (
-  `id` int NOT NULL DEFAULT '0',
+  `id` int NOT NULL AUTO_INCREMENT,
   `categoryId` int DEFAULT NULL,
-  `productId` int DEFAULT NULL
+  `productId` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table epsonlocal.request
+-- Dumping structure for table epsonlocaldb.request
 CREATE TABLE IF NOT EXISTS `request` (
-  `id` int NOT NULL DEFAULT '0',
+  `id` int NOT NULL AUTO_INCREMENT,
   `approvedTime` datetime DEFAULT NULL,
-  `approvedBy` int DEFAULT NULL,
+  `ApprovedBy` varchar(50) DEFAULT NULL,
   `createdOnUtc` datetime DEFAULT NULL,
-  `createdById` int DEFAULT NULL,
-  `updatedById` int DEFAULT NULL,
-  `productId` int DEFAULT NULL,
+  `createdById` longtext,
+  `updatedById` longtext,
   `updatedOnUtc` datetime DEFAULT NULL,
   `segment` varchar(40) DEFAULT NULL,
-  `managerId` int DEFAULT NULL,
-  `managerName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `quantity` int DEFAULT NULL,
   `priority` int DEFAULT NULL,
   `deadline` datetime DEFAULT NULL,
   `totalPrice` decimal(20,6) DEFAULT NULL,
-  `timeToResolution` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `TimeToResolution` time DEFAULT NULL,
+  `TotalBudget` decimal(20,6) NOT NULL DEFAULT '0.000000',
+  `ApprovalState` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table epsonlocal.roles
-CREATE TABLE IF NOT EXISTS `roles` (
-  `id` int NOT NULL DEFAULT '0',
-  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `normalizedName` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- Dumping structure for table epsonlocaldb.requestproduct
+CREATE TABLE IF NOT EXISTS `requestproduct` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `RequestId` int NOT NULL,
+  `ProductId` int NOT NULL,
+  `quantity` int DEFAULT NULL,
+  `FulfillerId` varchar(255) DEFAULT NULL,
+  `HasFulfilled` tinyint(1) NOT NULL DEFAULT '0',
+  `Price` decimal(20,6) NOT NULL DEFAULT '0.000000',
+  PRIMARY KEY (`Id`),
+  KEY `FK_RequestProduct_Request` (`RequestId`),
+  KEY `FK_RequestProduct_Product` (`ProductId`),
+  CONSTRAINT `FK_RequestProduct_Product` FOREIGN KEY (`ProductId`) REFERENCES `product` (`id`),
+  CONSTRAINT `FK_RequestProduct_Request` FOREIGN KEY (`RequestId`) REFERENCES `request` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table epsonlocal.user
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int NOT NULL DEFAULT '0',
-  `userName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `normalizedUserName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `normalizedEmail` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `passwordHash` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `phoneNumber` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `fullName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `isActive` bit(1) DEFAULT NULL,
-  `createdOnUtc` datetime DEFAULT NULL,
-  `updatedOnUtc` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- Dumping structure for table epsonlocaldb.slaholiday
+CREATE TABLE IF NOT EXISTS `slaholiday` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `Date` date NOT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  `IsAdhoc` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table epsonlocal.userrole
-CREATE TABLE IF NOT EXISTS `userrole` (
-  `id` int NOT NULL DEFAULT '0',
-  `user_id` int DEFAULT NULL,
-  `role_id` int DEFAULT NULL
+-- Dumping structure for table epsonlocaldb.slastaffleave
+CREATE TABLE IF NOT EXISTS `slastaffleave` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `StaffId` varchar(50) NOT NULL DEFAULT '',
+  `StartDate` date NOT NULL,
+  `EndDate` date NOT NULL,
+  `Reason` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table epsonlocaldb.__efmigrationshistory
+CREATE TABLE IF NOT EXISTS `__efmigrationshistory` (
+  `MigrationId` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `ProductVersion` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`MigrationId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
