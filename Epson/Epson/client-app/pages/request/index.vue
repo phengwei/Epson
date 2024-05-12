05be2bff0b4d52ee279edfb1eeb3e1f2ec2fea23
phengwei <56812779+phengwei@users.noreply.github.com>
2024-05-12 15:20:17 +0800

    .

--- Epson/Epson/client-app/pages/request/index.vue
+++ Epson/Epson/client-app/pages/request/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d-flex justify-content-center align-items-center vh-100" data-app="true" v-if="loggedInUser.roles.includes('Sales') || loggedInUser.roles.includes('Sales Section Head') || loggedInUser.roles.includes('Sales Operation')">
+  <div class="d-flex justify-content-center align-items-center vh-100" data-app="true">
     <v-card class="mx-auto" style="width: 90%">
       <v-card-title class="d-flex justify-content-between align-items-center">
         <span style="flex-grow: 1;">Request</span>
