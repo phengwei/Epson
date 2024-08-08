c418d0a4533ae0576b5f0cd1ba8f605ee267f105
phengwei <56812779+phengwei@users.noreply.github.com>
2024-08-09 01:37:55 +0800

    handle existing user when logging in from sso for the first time thru assertion api

--- Epson/Epson/Controllers/API/AuthApiController.cs
+++ Epson/Epson/Controllers/API/AuthApiController.cs
@@ -12,6 +12,10 @@ using Epson.Infrastructure;
 using Microsoft.Extensions.Options;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.Extensions.Logging;
+using Epson.Models.Users;
+using Epson.Services.Interface.Users;
+using Epson.Core.Domain.Users;
+using Microsoft.AspNetCore.Identity;
 
 namespace Epson.Controllers.API
 {
@@ -23,8 +27,10 @@ namespace Epson.Controllers.API
         private readonly IConfiguration configuration;
         private readonly Saml2Configuration config;
         private readonly Serilog.ILogger logger;
+        private readonly IJwtService _jwtService;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public AuthApiController(IOptions<Saml2Configuration> saml2Config, IConfiguration configuration, Serilog.ILogger logger)
+        public AuthApiController(IOptions<Saml2Configuration> saml2Config, IConfiguration configuration, Serilog.ILogger logger, IJwtService jwtService, UserManager<ApplicationUser> userManager)
         {
             this.configuration = configuration;
             this.logger = logger;
@@ -46,7 +52,8 @@ namespace Epson.Controllers.API
             {
                 config.SignatureValidationCertificates.Add(new X509Certificate2(certPath));
             }
-
+            _jwtService = jwtService;
+            _userManager = userManager;
         }
 
         [HttpGet("ssoLogin")]
@@ -61,30 +68,87 @@ namespace Epson.Controllers.API
         [HttpPost("AssertionConsumerService")]
         public async Task<IActionResult> AssertionConsumerService()
         {
-            var httpRequest = Request.ToGenericHttpRequest(validate: true);
-            var saml2AuthnResponse = new Saml2AuthnResponse(config);
-
-            logger.Information("Reading SAML response from HTTP request...");
-            httpRequest.Binding.ReadSamlResponse(httpRequest, saml2AuthnResponse);
-
-            if (saml2AuthnResponse.Status != Saml2StatusCodes.Success)
+            try
             {
-                logger.Error($"SAML Response status: {saml2AuthnResponse.Status}");
-                throw new AuthenticationException($"SAML Response status: {saml2AuthnResponse.Status}");
+                var httpRequest = Request.ToGenericHttpRequest(validate: true);
+                var saml2AuthnResponse = new Saml2AuthnResponse(config);
+
+                logger.Information("Reading SAML response from HTTP request...");
+                httpRequest.Binding.ReadSamlResponse(httpRequest, saml2AuthnResponse);
+
+                if (saml2AuthnResponse.Status != Saml2StatusCodes.Success)
+                {
+                    logger.Error($"SAML Response status: {saml2AuthnResponse.Status}");
+                    throw new AuthenticationException($"SAML Response status: {saml2AuthnResponse.Status}");
+                }
+
+                logger.Information("Unbinding SAML response from HTTP request...");
+                httpRequest.Binding.Unbind(httpRequest, saml2AuthnResponse);
+
+                logger.Information("Extracting claims from SAML response...");
+                var claims = saml2AuthnResponse.ClaimsIdentity.Claims;
+                var email = claims.FirstOrDefault(c => c.Type == "user.mail")?.Value;
+                var uniqueIdentifier = claims.FirstOrDefault(c => c.Type == "user.userprincipalname")?.Value;
+                var givenName = claims.FirstOrDefault(c => c.Type == "user.givenname")?.Value;
+                var surname = claims.FirstOrDefault(c => c.Type == "user.surname")?.Value;
+
+                if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(uniqueIdentifier))
+                {
+                    logger.Error("Email or Unique Identifier claim not found in SAML response.");
+                    return StatusCode(500, "Internal server error: Missing required claims.");
+                }
+
+                var user = await _userManager.FindByEmailAsync(email);
+                if (user != null)
+                {
+                    logger.Information($"User found for email {email}, updating SSO identifier if necessary...");
+                    if (string.IsNullOrEmpty(user.ssoIdentifier))
+                    {
+                        user.ssoIdentifier = uniqueIdentifier;
+                        await _userManager.UpdateAsync(user);
+                    }
+                }
+                else
+                {
+                    logger.Information($"No user found for email {email}, creating new user...");
+                    user = new ApplicationUser
+                    {
+                        UserName = email,
+                        Email = email,
+                        ssoIdentifier = uniqueIdentifier,
+                        firstName = givenName,
+                        lastName = surname
+                    };
+                    var result = await _userManager.CreateAsync(user);
+                    if (!result.Succeeded)
+                    {
+                        logger.Error($"Error creating new user: {string.Join(", ", result.Errors.Select(e => e.Description))}");
+                        return StatusCode(500, "Internal server error: Unable to create new user.");
+                    }
+                }
+
+                logger.Information("Creating session for the authenticated user...");
+                await saml2AuthnResponse.CreateSession(HttpContext,
+                    claimsTransform: (claimsPrincipal) => ClaimsTransform.Transform(claimsPrincipal));
+
+                var relayStateQuery = httpRequest.Binding.GetRelayStateQuery();
+                var returnUrl = relayStateQuery.ContainsKey(relayStateReturnUrl) ? relayStateQuery[relayStateReturnUrl] : Url.Content("~/");
+
+                logger.Information($"Generating JWT token for user {user.Email}...");
+                var generatedToken = await _jwtService.GenerateToken(user);
+
+                logger.Information($"Redirecting to {returnUrl}");
+                return Ok(new
+                {
+                    token = generatedToken,
+                    returnUrl = returnUrl
+                });
             }
-
-            logger.Information("Unbinding SAML response from HTTP request...");
-            httpRequest.Binding.Unbind(httpRequest, saml2AuthnResponse);
-
-            logger.Information("Creating session for the authenticated user...");
-            await saml2AuthnResponse.CreateSession(HttpContext,
-                claimsTransform: (claimsPrincipal) => ClaimsTransform.Transform(claimsPrincipal));
-
-            var relayStateQuery = httpRequest.Binding.GetRelayStateQuery();
-            var returnUrl = relayStateQuery.ContainsKey(relayStateReturnUrl) ? relayStateQuery[relayStateReturnUrl] : Url.Content("~/");
-
-            logger.Information($"Redirecting to {returnUrl}");
-            return Redirect(returnUrl);
+            catch (Exception ex)
+            {
+                logger.Error($"Error processing SAML response: {ex}");
+                return StatusCode(500, "Internal server error");
+            }
         }
 
 
